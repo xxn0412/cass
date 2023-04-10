b7e51a419f3b9b92d9c115ccc98502bac731e81a
x <1983375427@qq.com>
2023-04-10 14:27:15 +0800

    修改了App.vue文件

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <div>aaaaa</div>
+    <div>bbbbb</div>
+    <div>ccccc</div>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
